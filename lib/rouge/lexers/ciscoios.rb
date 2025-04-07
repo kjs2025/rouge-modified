40074004344bc656a75314bb647a78f54063a219
kpl2022 <kevinjsun2015@gmail.com>
2025-04-07 00:51:00 -0700

    sdf

---
+++ lib/rouge/lexers/ciscoios.rb
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*- #
+# frozen_string_literal: true
+
+module Rouge
+  module Lexers
+    class CiscoIOS < Lexer
+      title "Cisco IOS"
+      desc "A lexer for Cisco IOS configuration files"
+
+      tag 'cios'
+      aliases 'cios', 'cisco'
+      filenames '*.conf', '*.cfg', '*.ios'
+      mimetypes 'text/x-cisco-ios'
+
+      attr_reader :token
+      def initialize(*)
+        super
+
+        @token = token_option(:token) || Text
+      end
+
+      def stream_tokens(string, &b)
+        yield self.token, string
+      end
+    end
+  end
+end
